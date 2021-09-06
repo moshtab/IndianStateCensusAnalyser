8e45b2a8de1a47a40061f7836c42d4290af38bf4
moshtab <mdmohsin41439@gmail.com>
2021-09-06 23:08:34 +0530

    Readed csvData by CsvReader

--- src/main/java/com/day29/IndianStateCensusAnalyser/StateCensusAnalyser.java
+++ src/main/java/com/day29/IndianStateCensusAnalyser/StateCensusAnalyser.java
@@ -1,9 +1,34 @@
 package com.day29.IndianStateCensusAnalyser;
 
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.opencsv.CSVReader;
+
 public class StateCensusAnalyser {
+	private static final String File_Name = "src/main/java/resources/censusStateData.csv";
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Indian State census Analyser Program");
+		readCensusData();
+
+	}
+
+	private static void readCensusData() {
+		try (Reader reader = Files.newBufferedReader(Paths.get(File_Name));
+				CSVReader csvReader = new CSVReader(reader);) {
+			String[] arr;
+			while ((arr = csvReader.readNext()) != null) {
+				System.out.println("StateName :" + arr[0]);
+				System.out.println("CensusCounts :" + arr[1]);
+
+				System.out.println("====================");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
