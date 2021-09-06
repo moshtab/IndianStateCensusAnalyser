66c478d83bd80fce7c4bea5d4371ae77fbc64589
moshtab <mdmohsin41439@gmail.com>
2021-09-06 22:47:14 +0530

    Added msg

---
+++ src/main/java/com/day29/IndianStateCensusAnalyser/StateCensusAnalyser.java
@@ -0,0 +1,10 @@
+package com.day29.IndianStateCensusAnalyser;
+
+public class StateCensusAnalyser {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Indian State census Analyser Program");
+
+	}
+
+}
